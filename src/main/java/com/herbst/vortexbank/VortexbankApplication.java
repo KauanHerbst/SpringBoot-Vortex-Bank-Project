1b306a91332bf13d2cf318123e5c92e9caccadb6
KauanHerbst <kauanherbst7@gmail.com>
2024-07-04 14:00:31 -0300

    Created Project Setup

---
+++ src/main/java/com/herbst/vortexbank/VortexbankApplication.java
@@ -0,0 +1,13 @@
+package com.herbst.vortexbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexbankApplication.class, args);
+	}
+
+}
